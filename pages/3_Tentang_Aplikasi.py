8bc29bb91fd00c4f3973adaabb79fabd4b9479ae
Ferri Kusuma <cklothox79@gmail.com>
2025-07-07 11:35:53 +0700

    Create 3_Tentang_Aplikasi.py

---
+++ pages/3_Tentang_Aplikasi.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("📘 Tentang Aplikasi")
+st.markdown("""
+Aplikasi ini dibuat oleh **Ferri Kusuma (STMKG/M8TB_14.22.0003)**  
+untuk memantau dinamika atmosfer berdasarkan skala global, regional, dan lokal.
+
+📚 Cocok untuk edukasi meteorologi publik, sekolah, dan komunitas cuaca.
+""")
