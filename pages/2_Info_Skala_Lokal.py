25884fea5064806040a57be8a99966ae2c1c7672
Ferri Kusuma <cklothox79@gmail.com>
2025-07-07 11:35:21 +0700

    Create 2_Info_Skala_Lokal.py

---
+++ pages/2_Info_Skala_Lokal.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("🧭 Skala Atmosfer Lokal")
+st.markdown("""
+Berikut beberapa fenomena skala lokal yang memengaruhi cuaca:
+- 🌄 **Angin Lembah-Gunung**
+- 🌡️ **Konvergensi Mikro**
+- 🌆 **Efek Urban**
+""")
